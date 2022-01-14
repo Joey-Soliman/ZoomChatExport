414691dd62b1e89616d09d033b9224e2078d8e17
Joey-Soliman <79536865+Joey-Soliman@users.noreply.github.com>
2022-01-14 14:07:42 -0800

    Delete src/zoom directory

--- src/zoom/restapi/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restapi.restapi.repository;
-
-import com.example.restapi.restapi.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
